1ca964ad202f935e5bf5e9eb48ecbb3867e6b6bf
zhouang <316447676@qq.com>
2022-05-24 01:29:30 +0800

    add: 增加web基本功能

---
+++ sd-param/src/main/java/com/whitefly/param/web/vo/PersonVO.java
@@ -0,0 +1,16 @@
+package com.whitefly.param.web.vo;
+
+import lombok.Data;
+
+@Data
+public class PersonVO {
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 年龄
+     */
+    private Integer age;
+}
